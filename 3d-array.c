6a5a65f1f7357537684d067c3552bddef132fa87
rajansawhney <rajan.k.sawhney@gmail.com>
2023-10-02 15:22:28 -1000

    comment update

--- 3d-array.c
+++ 3d-array.c
@@ -3,6 +3,7 @@ Write a C Program that stores 12 values entered by the user and prints them with
 
 NOTE:
 1. Accept only integers
+3. Use a 3 dimensional array for your inputs
 2. Display output as:
     Value at [<i>][<j>][<k>] is: <value>
 */
